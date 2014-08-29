01c64fde5ae3a5f5c2586744711876aaf8985bf2
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-08-29 11:52:16 +0700

    add more methods to url_filter

--- lib/yoolk/instant_website/liquid/filters/url_filter.rb
+++ lib/yoolk/instant_website/liquid/filters/url_filter.rb
@@ -29,21 +29,83 @@ module Yoolk
           condition ? class_name : nil
         end
 
-        def link_to_home(value)
-          link_to value, root_path, value, toggle_class_name('active', current_page?(:root))
+        # Navigation links
+        def office_path
+          '/office'
         end
 
-        def root_path
+        def link_to_office(value)
+          link_to value, office_path, value
+        end
+
+        def home_path
           '/'
         end
 
+        def link_to_home(value)
+          link_to value, home_path, value, toggle_class_name('active', current_page?(home_path))
+        end
+
+        def galleries_path
+          '/galleries'
+        end
+
+        def link_to_galleries(value)
+          link_to value, galleries_path, value, toggle_class_name('active', current_page?(galleries_path))
+        end
+
+        def people_path
+          '/people'
+        end
+
+        def link_to_people(value)
+          link_to value, people_path, value, toggle_class_name('active', current_page?(people_path))
+        end
+
+        def products_path
+          '/products'
+        end
+
+        def link_to_products(value)
+          link_to value, products_path, value, toggle_class_name('active', current_page?(products_path))
+        end
+
+        def services_path
+          '/services'
+        end
+
+        def link_to_services(value)
+          link_to value, services_path, value, toggle_class_name('active', current_page?(services_path))
+        end
+
+        def foods_path
+          '/foods'
+        end
+
+        def link_to_foods(value)
+          link_to value, foods_path, value, toggle_class_name('active', current_page?(foods_path))
+        end
+
+        def announcements_path
+          '/announcements'
+        end
+
+        def link_to_announcements(value)
+          link_to value, announcements_path, value, toggle_class_name('active', current_page?(announcements_path))
+        end
+
+        def map_path
+          '/map'
+        end
+
+        def link_to_map(value)
+          link_to value, map_path, value, toggle_class_name('active', current_page?(map_path))
+        end
+
         # Return true if the input is the current page
-        # value: could be 'root', 'foods', 'products', 'services', 'contact_us', 'about_us'
-        def current_page?(value)
-          value = value.to_sym
-          if value == :root
-            @context.registers['controller'].request.fullpath == root_path
-          end
+        # path: could be 'home_path', 'foods_path', 'products_path', 'services_path', 'contact_us_path', 'about_us_path'
+        def current_page?(path)
+          @context.registers['controller'].request.fullpath == path
         end
       end
     end
--- spec/lib/yoolk/instant_website/liquid/filters/url_filter_spec.rb
+++ spec/lib/yoolk/instant_website/liquid/filters/url_filter_spec.rb
@@ -11,51 +11,105 @@ module Yoolk
           @context.add_filters(UrlFilter)
         end
 
-        it '#theme_asset_path' do
-          @context['url'] = 'all.js'
+        context 'general' do
+          it '#theme_asset_path' do
+            @context['url'] = 'all.js'
 
-          @context.registers['helper'].should_receive(:asset_path).with('theme_a/all.js')
-          ::Liquid::Variable.new('url | theme_asset_path').render(@context)
+            @context.registers['helper'].should_receive(:asset_path).with('theme_a/all.js')
+            ::Liquid::Variable.new('url | theme_asset_path').render(@context)
+          end
+
+          it '#script_tag' do
+            @context['url'] = 'jquery.js'
+
+            expect(::Liquid::Variable.new('url | script_tag').render(@context)).to eq(%|<script src="jquery.js" type="text/javascript"></script>|)
+          end
+
+          it '#stylesheet_tag' do
+            @context['url'] = 'bootstrap.css'
+
+            expect(::Liquid::Variable.new('url | stylesheet_tag').render(@context)).to eq(%|<link href="bootstrap.css" rel="stylesheet" type="text/css" media="all" />|)
+          end
+
+          it '#link_to' do
+            @context['link'] = 'Google'
+
+            expect(::Liquid::Variable.new("link | link_to: 'http://www.google.com.kh', 'google'").render(@context)).to eq(%|<a href="http://www.google.com.kh" title="google" class="">Google</a>|)
+          end
+
+          it '#image_tag' do
+            @context['url'] = 'https://www.google.com.kh/images/srpr/logo11w.png'
+
+            expect(::Liquid::Variable.new('url | image_tag').render(@context)).to eq(%|<img src="https://www.google.com.kh/images/srpr/logo11w.png" alt="" />|)
+          end
         end
 
-        it '#script_tag' do
-          @context['url'] = 'jquery.js'
+        context 'link_to methods' do
+          before do
+            allow_message_expectations_on_nil
+          end
 
-          expect(::Liquid::Variable.new('url | script_tag').render(@context)).to eq(%|<script src="jquery.js" type="text/javascript"></script>|)
-        end
+          it '#link_to_office' do
+            @context['name'] = 'Office'
 
-        it '#stylesheet_tag' do
-          @context['url'] = 'bootstrap.css'
+            expect(::Liquid::Variable.new('name | link_to_office').render(@context)).to eq(%|<a href="/office" title="Office" class="">Office</a>|)
+          end
 
-          expect(::Liquid::Variable.new('url | stylesheet_tag').render(@context)).to eq(%|<link href="bootstrap.css" rel="stylesheet" type="text/css" media="all" />|)
-        end
+          it "#link_to_home set 'active' class" do
+            @context['name'] = 'Home'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/')
 
-        it '#link_to' do
-          @context['link'] = 'Google'
+            expect(::Liquid::Variable.new('name | link_to_home').render(@context)).to eq(%|<a href="/" title="Home" class="active">Home</a>|)
+          end
 
-          expect(::Liquid::Variable.new("link | link_to: 'http://www.google.com.kh', 'google'").render(@context)).to eq(%|<a href="http://www.google.com.kh" title="google" class="">Google</a>|)
-        end
+          it "#link_to_home doesn't set 'active' class" do
+            @context['name'] = 'Home'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/else')
 
-        it '#image_tag' do
-          @context['url'] = 'https://www.google.com.kh/images/srpr/logo11w.png'
+            expect(::Liquid::Variable.new('name | link_to_home').render(@context)).to eq(%|<a href="/" title="Home" class="">Home</a>|)
+          end
 
-          expect(::Liquid::Variable.new('url | image_tag').render(@context)).to eq(%|<img src="https://www.google.com.kh/images/srpr/logo11w.png" alt="" />|)
-        end
+          it '#link_to_galleries' do
+            @context['name'] = 'Galleries'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/galleries')
 
-        it "#link_to_home set 'active' class" do
-          allow_message_expectations_on_nil
-          @context['name'] = 'Home'
-          @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/')
+            expect(::Liquid::Variable.new('name | link_to_galleries').render(@context)).to eq(%|<a href="/galleries" title="Galleries" class="active">Galleries</a>|)
+          end
 
-          expect(::Liquid::Variable.new('name | link_to_home').render(@context)).to eq(%|<a href="/" title="Home" class="active">Home</a>|)
-        end
+          it '#link_to_people' do
+            @context['name'] = 'People'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/people')
 
-        it "#link_to_home doesn't set 'active' class" do
-          allow_message_expectations_on_nil
-          @context['name'] = 'Home'
-          @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/else')
+            expect(::Liquid::Variable.new('name | link_to_people').render(@context)).to eq(%|<a href="/people" title="People" class="active">People</a>|)
+          end
 
-          expect(::Liquid::Variable.new('name | link_to_home').render(@context)).to eq(%|<a href="/" title="Home" class="">Home</a>|)
+          it '#link_to_products' do
+            @context['name'] = 'Products'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/products')
+
+            expect(::Liquid::Variable.new('name | link_to_products').render(@context)).to eq(%|<a href="/products" title="Products" class="active">Products</a>|)
+          end
+
+          it '#link_to_services' do
+            @context['name'] = 'Services'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/services')
+
+            expect(::Liquid::Variable.new('name | link_to_services').render(@context)).to eq(%|<a href="/services" title="Services" class="active">Services</a>|)
+          end
+
+          it '#link_to_announcements' do
+            @context['name'] = 'Announcements'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/announcements')
+
+            expect(::Liquid::Variable.new('name | link_to_announcements').render(@context)).to eq(%|<a href="/announcements" title="Announcements" class="active">Announcements</a>|)
+          end
+
+          it '#link_to_map' do
+            @context['name'] = 'Map'
+            @context.registers['controller'].stub_chain(:request, :fullpath).and_return('/map')
+
+            expect(::Liquid::Variable.new('name | link_to_map').render(@context)).to eq(%|<a href="/map" title="Map" class="active">Map</a>|)
+          end
         end
       end
     end
