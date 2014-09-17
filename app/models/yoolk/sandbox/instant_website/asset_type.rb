7ecc7517d043d68c1ee528ed4591e4371abb920b
radin-reth <radin.reth@gmail.com>
2014-09-17 16:35:14 +0700

    add instant website sandbox model

---
+++ app/models/yoolk/sandbox/instant_website/asset_type.rb
@@ -0,0 +1,9 @@
+module Yoolk
+  module Sandbox
+    class InstantWebsite::AssetType < Yoolk::Sandbox::Base
+
+      attribute :id,            String
+      attribute :name,          String
+    end
+  end
+end
