191785e7cfe3fb8ac1276ba138c19f3475119f72
radin-reth <radin.reth@gmail.com>
2014-09-17 08:34:00 +0700

    add people method group by position level manager, director, employee

--- app/drops/yoolk/liquid/listing/people_drop.rb
+++ app/drops/yoolk/liquid/listing/people_drop.rb
@@ -3,8 +3,10 @@ module Yoolk
     class Listing::PeopleDrop < ::Liquid::Rails::CollectionDrop
       # scope :emails
 
-      def director
-        self.class.new people.select{|person| person.position_level.name.downcase == 'director'}
+      %w(director manager employee).each do |position_level_method|
+        define_method(position_level_method) do
+          self.class.new people.select{|person| person.position_level.name.downcase == position_level_method }
+        end
       end
 
       def people
