85c1659c44c114090ae0362aec6deb8242172c4c
radin-reth <radin.reth@gmail.com>
2014-09-18 08:49:48 +0700

    [#77992622]:zap: update people class scoping by position level

--- app/drops/yoolk/liquid/listing/people_drop.rb
+++ app/drops/yoolk/liquid/listing/people_drop.rb
@@ -1,25 +1,8 @@
 module Yoolk
   module Liquid
     class Listing::PeopleDrop < ::Liquid::Rails::CollectionDrop
-      # scope :emails
+      scope :directors, :managers, :employees
 
-      def [](index)
-        dropped_collection[index.to_i]
-      end
-
-      %w(director manager employee).each do |position_level_method|
-        define_method(position_level_method) do
-          self.class.new people.select{|person| person.position_level.name.downcase == position_level_method }
-        end
-      end
-
-      def to_liquid
-        self
-      end
-
-      def people
-        objects
-      end
     end
   end
 end
