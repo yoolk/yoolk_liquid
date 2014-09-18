6c4065531eb7f7e4f93ba9853b89a0736dc6afdc
radin-reth <radin.reth@gmail.com>
2014-09-18 14:12:15 +0700

    [#77992622] remove portal unnecessary attribute

--- app/models/yoolk/sandbox/instant_website/asset_type.rb
+++
@@ -1,9 +0,0 @@
-module Yoolk
-  module Sandbox
-    class InstantWebsite::AssetType < Yoolk::Sandbox::Base
-
-      attribute :id,            String
-      attribute :name,          String
-    end
-  end
-end
