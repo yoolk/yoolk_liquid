96ee1c9464cca9fe88251a5b49f4e5b56adc8869
radin-reth <radin.reth@gmail.com>
2014-09-18 15:53:32 +0700

    add media drop spec

--- spec/drops/yoolk/liquid/listing/media_drop_spec.rb
+++ spec/drops/yoolk/liquid/listing/media_drop_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 module Yoolk
   module Liquid
     describe Listing::MediaDrop do
+
       it { should have_attribute(:id) }
       it { should have_attribute(:embed_script) }
       it { should have_attribute(:is_active) }
