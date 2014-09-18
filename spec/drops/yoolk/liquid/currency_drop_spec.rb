4097e811ff2e8d68fb09b571de0eb72511db564b
radin-reth <radin.reth@gmail.com>
2014-09-18 18:05:00 +0700

    [#77992622] add currency, category drop, currency model spec

---
+++ spec/drops/yoolk/liquid/currency_drop_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+module Yoolk
+  module Liquid
+    describe CurrencyDrop do
+
+      it { should have_attribute(:id) }
+      it { should have_attribute(:code) }
+    end
+  end
+end
