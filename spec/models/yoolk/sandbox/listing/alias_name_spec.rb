0131c619b120ce0c49bb7385c4252e8f6a2c16d6
radin-reth <radin.reth@gmail.com>
2014-09-18 16:38:06 +0700

    add alias name spec

---
+++ spec/models/yoolk/sandbox/listing/alias_name_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+module Yoolk
+  module Sandbox
+    describe Listing::AliasName do
+      it 'has :id attribute' do
+        expect(described_class).to have_attribute(:id).of_type(String)
+      end
+
+      it 'has :name attribute' do
+        expect(described_class).to have_attribute(:name).of_type(String)
+      end
+
+      it 'has :type attribute' do
+        expect(described_class).to have_attribute(:type).of_type(String)
+      end
+
+      it 'has :created_at attribute' do
+        expect(described_class).to have_attribute(:created_at).of_type(DateTime)
+      end
+
+      it 'has :updated_at attribute' do
+        expect(described_class).to have_attribute(:updated_at).of_type(DateTime)
+      end
+    end
+  end
+end
