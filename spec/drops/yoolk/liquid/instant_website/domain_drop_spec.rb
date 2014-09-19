f93eec57450107d2514a81138278a5cae762d12c
radin-reth <radin.reth@gmail.com>
2014-09-19 14:39:03 +0700

    [#77992622] add domain, template, template cover photo drop spec

---
+++ spec/drops/yoolk/liquid/instant_website/domain_drop_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+module Yoolk
+  module Liquid
+    describe InstantWebsite::DomainDrop do
+      it { should have_attribute(:id) }
+      it { should have_attribute(:name) }
+      it { should have_attribute(:type) }
+      it { should have_attribute(:google_analytics_key) }
+      it { should have_attribute(:is_primary) }
+      it { should have_attribute(:created_at) }
+      it { should have_attribute(:updated_at) }
+    end
+  end
+end
