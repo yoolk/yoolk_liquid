96a294bffd924742013941dd6d9a26e8cac4e1d8
radin-reth <radin.reth@gmail.com>
2014-09-30 12:01:14 +0700

    add yoolk liquid form tag

--- app/tags/yoolk/liquid/form_tag.rb
+++ app/tags/yoolk/liquid/form_tag.rb
@@ -1,19 +1,41 @@
 module Yoolk
   module Liquid
     class FormTag < ::Liquid::Block
-      def initialize(tag_name, markup, tokens)
-         super
-         @url = markup
-      end
-      def render(context)
-        @token ||= context.registers[:view].form_authenticity_token
-        @flash = "<p>#{context.registers[:view].flash[:notice]}<p>"
+      Syntax = /(#{::Liquid::VariableSignature}+)/
 
-        "<form accept-charset='UTF-8' method='post' #{@url}>
-        <div style='margin:0;padding:0;display:inline'>
-        <input name='utf8' type='hidden' value='✓'>
-        <input name='authenticity_token' type='hidden' value='#{@token}'>
-        </div>" + @flash + super + "</form>"
+      def initialize(tag_name, markup, options)
+        super
+
+        if markup =~ Syntax
+          @variable_name = $1
+          @attributes    = {}
+        else
+          raise SyntaxError.new("Syntax Error - Valid syntax: {% form contact %}")
+        end
+      end
+
+      def render(context)
+        @csrf_token  = context.registers[:view].form_authenticity_token
+        @flash       = context.registers[:view].flash[:notice]
+        contact_from = context[@variable_name]
+        context.stack do
+          context['form'] = {
+            'errors' => @flash
+          }
+          form_builder(contact_from, render_all(@nodelist, context))
+        end
+      end
+
+      def form_builder(contact_from, input)
+        %Q{
+          <form accept-charset='UTF-8' method='post' class='contact-form' id='contact-form' action=''>
+            <div style='margin:0;padding:0;display:inline'>
+              <input name='utf8' type='hidden' value='✓'>
+              <input name='authenticity_token' type='hidden' value='#{@csrf_token}'>
+            </div>
+            #{input}
+          </form>
+        }
       end
 
     end
