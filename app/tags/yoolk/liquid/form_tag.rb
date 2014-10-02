6121015deccfcb86d1f1a61da18e3b8f73b81304
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-02 15:33:47 +0700

    implement form_tag

--- app/tags/yoolk/liquid/form_tag.rb
+++ app/tags/yoolk/liquid/form_tag.rb
@@ -8,34 +8,67 @@ module Yoolk
 
         if markup =~ Syntax
           @variable_name = $1
+          unless @variable_name.in?(['contact', 'feedback', 'reservation'])
+            raise ArgumentError, 'valid parameters for form tag are: contact, feedback, and reservation'
+          end
         else
-          raise SyntaxError.new("Syntax Error - Valid syntax: {% form contact %}")
+          raise SyntaxError.new("Syntax Error - Valid syntax: {% form 'contact' %}")
         end
       end
 
       def render(context)
-        @csrf_token  = context.registers[:view].form_authenticity_token
-        @flash       = context.registers[:view].flash[:notice]
+        @context = context
+        @object  = context[@variable_name]
+
         context.stack do
           context['form'] = {
-            'errors' => @flash
+            'errors' => @object.errors.blank? ? nil : ActiveModel::ErrorsDrop.new(@object.errors)
           }
+          @object.attributes.each do |field, message|
+            context['form'][field.to_s] = message
+          end
+
           form_builder(render_all(@nodelist, context))
         end
       end
 
       def form_builder(input)
         %Q{
-          <form accept-charset='UTF-8' method='post' class='contact-form' id='contact-form' action=''>
+          <form accept-charset='UTF-8' method='post' class='#{class_name}' id='#{class_name}' action='#{action_path}'>
             <div style='margin:0;padding:0;display:inline'>
               <input name='utf8' type='hidden' value='✓'>
-              <input name='authenticity_token' type='hidden' value='#{@csrf_token}'>
+              <input name='authenticity_token' type='hidden' value='#{csrf_token}'>
             </div>
             #{input}
           </form>
         }
       end
 
+      private
+
+        def action_path
+          if @object.is_a?(Yoolk::Form::Contact)
+            @context.registers[:controller].contact_us_path
+          end
+        end
+
+        def error_notification
+          'error_notification'
+        end
+
+        def class_name
+          if @object.is_a?(Yoolk::Form::Contact)
+            'comment-form'
+          end
+        end
+
+        def csrf_token
+          @context.registers[:view].form_authenticity_token
+        end
+
+        def flash
+          @context.registers[:view].flash
+        end
     end
   end
 end
