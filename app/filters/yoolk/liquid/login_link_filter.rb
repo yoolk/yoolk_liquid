1b617a7e7b5aa63fdddb0c1bdf66c1028a0c5cd3
radin-reth <radin.reth@gmail.com>
2014-10-02 14:55:12 +0700

    remove unuse comment code

--- app/filters/yoolk/liquid/login_link_filter.rb
+++ app/filters/yoolk/liquid/login_link_filter.rb
@@ -6,9 +6,6 @@ module Yoolk
 
         if @context.registers[:controller].params[:login].present?
           link_to image_tag(asset_path('sample/male-office.png'), class: "office-img").safe_concat("My Office"), nil, class: "office-account"
-          # link_to "/office/signin", {class: "office-account"} do
-          #   "my office"#image_tag(asset_url('sample/male-office.png'), class: "office-img").safe_concat("My Office")
-          # end
         else
           link_to "signin", "/office", class: "office-account"
         end
