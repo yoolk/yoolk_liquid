aca374a6195d1de339164fbd214ae41fa5547ec8
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-03 13:24:33 +0700

    fix facebook/twitter model

--- app/models/yoolk/sandbox/facebook/page.rb
+++ app/models/yoolk/sandbox/facebook/page.rb
@@ -1,18 +1,20 @@
 module Yoolk
   module Sandbox
-    class Facebook::Page < Yoolk::Sandbox::Base
+    module Facebook
+      class Page < Yoolk::Sandbox::Base
 
-      attribute :id,                       String
-      attribute :page_id,                  String
-      attribute :page_url,                 String
-      attribute :page_name,                String
-      attribute :page_access_token,        String
-      attribute :cover_photo_url,          String
-      attribute :installed_page_ids,       Array
-      attribute :photo_uploaded_status,    String
-      attribute :is_published,             Boolean
-      attribute :last_photo_uploaded_at,   DateTime
+        attribute :id,                       String
+        attribute :page_id,                  String
+        attribute :page_url,                 String
+        attribute :page_name,                String
+        attribute :page_access_token,        String
+        attribute :cover_photo_url,          String
+        attribute :installed_page_ids,       Array
+        attribute :photo_uploaded_status,    String
+        attribute :is_published,             Boolean
+        attribute :last_photo_uploaded_at,   DateTime
 
+      end
     end
   end
 end
