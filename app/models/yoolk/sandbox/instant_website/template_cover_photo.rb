05c3afb789556a4391ed4b76af6651cde545a070
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-03 14:37:05 +0700

    fix sandbox iw model

--- app/models/yoolk/sandbox/instant_website/template_cover_photo.rb
+++ app/models/yoolk/sandbox/instant_website/template_cover_photo.rb
@@ -1,13 +1,15 @@
 module Yoolk
   module Sandbox
-    class InstantWebsite::TemplateCoverPhoto < Yoolk::Sandbox::Base
+    module InstantWebsite
+      class TemplateCoverPhoto < Yoolk::Sandbox::Base
 
-      attribute :id,                    String
-      attribute :dimension,             String
-      attribute :created_at,            DateTime
-      attribute :updated_at,            DateTime
+        attribute :id,                    String
+        attribute :dimension,             String
+        attribute :created_at,            DateTime
+        attribute :updated_at,            DateTime
 
-      attribute :image,                 Yoolk::Sandbox::Attachment
+        attribute :image,                 Yoolk::Sandbox::Attachment
+      end
     end
   end
 end
