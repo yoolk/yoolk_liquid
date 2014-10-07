866cb5d08c8157a8105e44775968e56b472df1f5
radin-reth <radin.reth@gmail.com>
2014-10-07 16:27:31 +0700

    add logic draft stamp

--- app/tags/yoolk/liquid/draft_stamp_tag.rb
+++ app/tags/yoolk/liquid/draft_stamp_tag.rb
@@ -17,8 +17,30 @@ module Yoolk
       end
 
       def render(context)
-        image = context.registers[:helper].asset_url @variable_name
-        context.registers[:view].image_tag(image, style: "position: absolute; top: 0; right: 0; z-index: 9999;")
+        @context = context
+
+        if draft_stamp?
+          image = context.registers[:helper].asset_url @variable_name
+          context.registers[:view].image_tag(image, style: "position: absolute; top: 0; right: 0; z-index: 9999;")
+        end
+      end
+
+    private
+      def draft_stamp?
+        !is_live? && current_domain.try(:type) != "subdomain"
+      end
+
+      def is_live?
+        @context["listing"].instant_website.is_live
+      end
+
+      def current_domain
+        domains = @context["listing"].instant_website.domains.select do |domain|
+          domain.name == @context.registers[:controller].request.host
+        end
+        return nil if domains.nil?
+
+        domains.first
       end
 
     end
