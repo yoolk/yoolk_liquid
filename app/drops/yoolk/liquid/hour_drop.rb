4524360e91b61b4bdd43c5163d999017470462ef
radin-reth <radin.reth@gmail.com>
2014-10-22 17:25:55 +0700

    add to time method in HourDrop

--- app/drops/yoolk/liquid/hour_drop.rb
+++ app/drops/yoolk/liquid/hour_drop.rb
@@ -3,6 +3,11 @@ module Yoolk
     class HourDrop < BaseDrop
       attributes  :id, :hour, :minute, :created_at, :updated_at
 
+      def to_time
+        t  = "#{hour}:#{minute}"
+
+        Time.parse(t).strftime("%I:%M %p")
+      end
     end
   end
 end
