6918324a74562f3022572c12f0bcda356bf4bb6f
radin-reth <radin.reth@gmail.com>
2014-10-23 11:05:16 +0700

    update opening hour spec model

--- spec/models/yoolk/sandbox/listing/business_hour_spec.rb
+++ spec/models/yoolk/sandbox/listing/business_hour_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 module Yoolk
   module Sandbox
-    describe Listing::OpeningHour do
+    describe Listing::BusinessHour do
       it 'has :id attribute' do
         expect(described_class).to have_attribute(:id).of_type(Integer)
       end
