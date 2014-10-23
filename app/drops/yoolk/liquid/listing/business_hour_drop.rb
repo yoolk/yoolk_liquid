47895e7e750690ccc721770e4c585e8c96b34a56
radin-reth <radin.reth@gmail.com>
2014-10-23 15:18:13 +0700

    add 24h checking day

--- app/drops/yoolk/liquid/listing/business_hour_drop.rb
+++ app/drops/yoolk/liquid/listing/business_hour_drop.rb
@@ -5,6 +5,11 @@ module Yoolk
 
       belongs_to  :open,     with: 'Yoolk::Liquid::HourDrop'
       belongs_to  :close,    with: 'Yoolk::Liquid::HourDrop'
+
+      def full_day?
+        open.hour.to_i  + close.hour.to_i == 23 && \
+        open.minute.to_i + close.minute.to_i == 59
+      end
     end
   end
 end
