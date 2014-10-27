8401f6a4e563ca0fda21952e4b67a615632855a6
radin-reth <radin.reth@gmail.com>
2014-10-27 15:28:49 +0700

    add template cover photo logic

--- app/drops/yoolk/liquid/instant_website/website_drop.rb
+++ app/drops/yoolk/liquid/instant_website/website_drop.rb
@@ -23,40 +23,49 @@ module Yoolk
 
           # website_cover_photos.presence || template_cover_photos
           template_cover_photos
+          binding.pry
         end
-        private
 
-          def website_cover_photos
-            if template.try(:cover_photo).present?
-              object.cover_photos.select do |cover|
-                (cover.to_liquid.dimension_width == template.cover_photo.width) && \
-                (cover.to_liquid.dimension_height == template.cover_photo.height)
-              end
-            else
-              []
+        def website_cover_photos
+          if template.try(:cover_photo).present?
+            object.cover_photos.select do |cover|
+              (cover.to_liquid.dimension_width == template.cover_photo.width) && \
+              (cover.to_liquid.dimension_height == template.cover_photo.height)
             end
+          else
+            []
           end
+        end
 
-          def template_cover_photos
-            if true#h.preview_mode?
-              binding.pry
-              template = if request.params[:theme].present?
-                Mongoid.raise_not_found_error = false
-                Yoolk::Mongo::InstantWebsite::Template.find_by(name: h.params[:theme])
+        def template_cover_photos
+          if true#h.preview_mode?
+            template = {}
+            if request.params[:theme].present?
+              path = Rails.root.join('db', 'samples', 'jsons', 'templates/*')
+
+              Dir[path].each do |file|
+                return nil unless File.exists? file
+                attributes = Oj.load(File.read(file))
+                template = attributes if attributes["name"] == request.params[:theme]
               end
-              iw = h.current_listing.instant_website.send(:clone)
-              iw.template = template if template.present?
-              [iw.template.cover_photo].compact
-            elsif template.present?
-              [template.cover_photo].compact
-            else
-              []
             end
-          end
 
-          def current_host
-            request.host.to_s.gsub(/^www\./, '')
+            template_sandbox = Yoolk::Sandbox::InstantWebsite::Template.new(template)
+
+            object.template = template_sandbox if template.present?
+            [object.template.to_liquid.cover_photo].compact
+          elsif template.present?
+            [template.cover_photo].compact
+          else
+            []
           end
+        end
+
+        def current_host
+          request.host.to_s.gsub(/^www\./, '')
+        end
+
+
       end
     end
   end
