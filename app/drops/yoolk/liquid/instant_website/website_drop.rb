90b554fbd530167f85515ea29f615220a48cc59a
radin-reth <radin.reth@gmail.com>
2014-10-27 10:23:23 +0700

    check template preview

--- app/drops/yoolk/liquid/instant_website/website_drop.rb
+++ app/drops/yoolk/liquid/instant_website/website_drop.rb
@@ -21,8 +21,8 @@ module Yoolk
 
         def cover_photos
 
-          # website_cover_photos.presence || [object.template.cover_photo]
-          binding.pry
+          # website_cover_photos.presence || template_cover_photos
+          template_cover_photos
         end
         private
 
@@ -32,6 +32,23 @@ module Yoolk
                 (cover.to_liquid.dimension_width == template.cover_photo.width) && \
                 (cover.to_liquid.dimension_height == template.cover_photo.height)
               end
+            else
+              []
+            end
+          end
+
+          def template_cover_photos
+            if true#h.preview_mode?
+              binding.pry
+              template = if request.params[:theme].present?
+                Mongoid.raise_not_found_error = false
+                Yoolk::Mongo::InstantWebsite::Template.find_by(name: h.params[:theme])
+              end
+              iw = h.current_listing.instant_website.send(:clone)
+              iw.template = template if template.present?
+              [iw.template.cover_photo].compact
+            elsif template.present?
+              [template.cover_photo].compact
             else
               []
             end
