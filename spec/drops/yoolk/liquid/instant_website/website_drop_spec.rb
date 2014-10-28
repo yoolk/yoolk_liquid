4b2126bbc96f3d91dc2b71b72ef813f2ea945906
radin-reth <radin.reth@gmail.com>
2014-10-28 12:01:01 +0700

    test cover photo object size

--- spec/drops/yoolk/liquid/instant_website/website_drop_spec.rb
+++ spec/drops/yoolk/liquid/instant_website/website_drop_spec.rb
@@ -20,8 +20,15 @@ module Yoolk
 
     describe InstantWebsite::WebsiteDrop do
       subject { described_class.new(double) }
+      let(:listing) { Yoolk::Sandbox::Listing.new({'instant_website' => { 'template' => { 'cover_photo' => { 'dimension' => '1140x422', 'image' => { 'attachment_meta' => { 'original' => { 'width' => '1140', 'height' => '422'} } } } }, 'cover_photos' => [{ 'dimension' => '1140x422' }, { 'dimension' => '994x368' }]}}) }
 
       it { should respond_to(:office_url) }
+
+      it "#website_cover_photos: return matched cover photos to template cover photos" do
+        expect(listing.instant_website.cover_photos.count).to eq(2)
+      end
+      it "#template_cover_photos" do
+      end
     end
   end
 end
