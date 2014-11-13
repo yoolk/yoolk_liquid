90f7a0c09bdc92b907d764c7929d3bf1007a7c18
radin-reth <radin.reth@gmail.com>
2014-11-13 11:16:06 +0700

    add contact communications

--- app/drops/yoolk/liquid/listing/communication_drop.rb
+++ app/drops/yoolk/liquid/listing/communication_drop.rb
@@ -2,6 +2,85 @@ module Yoolk
   module Liquid
     class Listing::CommunicationDrop < BaseDrop
       attributes :id, :type, :label, :value, :is_first_line, :image_url, :created_at, :updated_at
+
+      def type_downcase
+        type.downcase
+      end
+
+      def li
+        if type_downcase.in?(["branch office", "department"])
+          branch_office
+        elsif value.blank?
+          h.content_tag :span, label
+        elsif object.email?
+          email_li
+        elsif object.website? || type_downcase.in?(["facebook", "twitter"])
+          website_link(social_network_value, value_with_http)
+        else
+          list_item
+        end
+      end
+
+      def value_with_http
+        return value if value =~ /^https?:\/\//
+
+        "http://#{value}"
+      end
+
+      def communication
+        object
+      end
+
+      private
+
+        def list_item(&block)
+          content = h.capture(&block) if block_given?
+
+          h.content_tag :li do
+            h.concat h.content_tag(:span, label.presence || type)
+            if content.present?
+              h.concat h.content_tag(:span, h.truncate(value), title: value) { content }
+            else
+              h.concat h.content_tag(:span, h.truncate(value), title: value)
+            end
+          end
+        end
+
+        def branch_office
+          if value.blank?
+            h.content_tag :span, label
+          else
+            list_item
+          end
+        end
+
+        def email_li
+          list_item do
+            h.content_tag(:a, value, href: "mailto:#{value}", rel: "tooltip", title: value) do
+              value
+            end
+          end
+        end
+
+        def website_link(value, href)
+          list_item do
+            h.content_tag(:a, h.truncate(value), href: href, rel: "tooltip", title: value)
+          end
+        end
+
+        def social_network_value
+          if type_downcase == "facebook"
+            value.gsub(/^.*facebook.com\/(?:pages\/)?([^\/]*)(\/.*)?$/, '\1')
+          elsif type_downcase == "twitter"
+            value.gsub(/^.*twitter.com\/([^\/]*)(\/.*)?$/, '\1')
+          else
+            value
+          end
+        end
+
+        def h
+          @context.registers[:helper]
+        end
     end
   end
 end
--- app/models/yoolk/sandbox/listing/communication.rb
+++ app/models/yoolk/sandbox/listing/communication.rb
@@ -10,15 +10,15 @@ module Yoolk
       attribute :updated_at,    DateTime
 
       def email?
-        type_downcase == 'e-mail'
+        type.downcase == 'e-mail'
       end
 
       def website?
-        type_downcase == 'website'
+        type.downcase == 'website'
       end
 
       def telephone?
-        type_downcase.in? ['tel', 'tel & fax', 'fax', 'mobile', 'mobile phone', 'toll free']
+        type.downcase.in? ['tel', 'tel & fax', 'fax', 'mobile', 'mobile phone', 'toll free']
       end
     end
   end
