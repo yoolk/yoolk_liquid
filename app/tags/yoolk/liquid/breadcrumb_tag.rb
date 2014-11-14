a3374b141fd945747016ef9d67fa08905cf37560
radin-reth <radin.reth@gmail.com>
2014-11-14 14:56:39 +0700

    update breadcrumb tag

--- app/tags/yoolk/liquid/breadcrumb_tag.rb
+++ app/tags/yoolk/liquid/breadcrumb_tag.rb
@@ -1,5 +1,6 @@
 module Yoolk
   module Liquid
+
     class BreadcrumbTag < ::Liquid::Tag
 
       Syntax = /(#{::Liquid::QuotedString})\s*class_active\s*=\s*(#{::Liquid::QuotedString})/
@@ -26,6 +27,26 @@ module Yoolk
           elsif reservation_page?   then list translate 'reservation'
           elsif feedback_page?      then list translate 'feedback'
           elsif contact_us_page?    then list translate 'contact_us'
+          else
+            if collection_category?
+              collection_with_category
+              if item_detail?
+                category = unless announcements? or galleries?
+                  view.content_tag :li do
+                    view.link_to category_namea, category_url
+                  end
+                end
+                if category
+                  category.concat( view.content_tag :li, item_detail )
+                else
+                  view.content_tag :li, item_detail
+                end
+              else
+                category_namea
+              end
+            else
+              list_home.concat( view.content_tag :li,  collection_name )
+            end
           end
 
           %Q{
@@ -36,20 +57,86 @@ module Yoolk
         end
       end
 
-      def list( text, url = "" )
-        home_path = view.content_tag :li do
+      def object
+        controller_base_name = controller.params['controller'].split("/")[0]
+        @controller_name =  if controller_base_name == 'menu'
+                              'foods'
+                            else
+                              controller_base_name
+                            end
+
+        collection = "listing.#{@controller_name}"
+        @object = @context[collection].select do |coll|
+          coll.id.to_i == controller.params["id"].to_i
+        end
+      end
+
+      def category_url
+        path =  if controller.params['controller'] == "menu/foods"
+                  "menu"
+                else
+                  controller.params['controller']
+                end
+
+        if path != 'galleries'
+          eval "view.#{path}_category_url(object.first.category)"
+        else
+          object.first.name
+        end
+      end
+
+      def item_detail
+        controller.params['controller'].split("/")[0] != 'announcements' ? object.first.name : object.first.id
+      end
+
+      def announcements?
+        controller.params['controller'] == 'announcements'
+      end
+
+      def galleries?
+        controller.params['controller'] == 'galleries'
+      end
+
+      def category_namea
+        controller.params["category_id"] || controller.params["id"]
+      end
+
+      def item_detail?
+        !controller.controller_path.ends_with?("categories") and controller.controller_path != 'galleries' and controller.action_name == 'show'
+      end
+
+      def list_home
+        view.content_tag :li do
           view.link_to translate('home'), view.root_path
         end
+      end
 
-        home_path.concat view.content_tag :li, text
+      def list( text, url = "" )
+        list_home.concat view.content_tag :li, text
+      end
+
+      def collection_with_category
+        list_cat = view.content_tag :li do
+          view.link_to collection_name, collection_url
+        end
+        list_home.concat list_cat
+      end
+
+      def collection_url
+        postfix = (collection_name == 'menu') ? '_index_path' : '_path'
+        eval('view.' + collection_name + postfix)
+      end
+
+      def collection_name
+        controller.controller_path.split("/")[0]
       end
 
       def translate key
-        I18n::t "#{request.params[:theme]}.links.#{key}"
+        I18n::t "#{controller.request.params[:theme]}.links.#{key}"
       end
 
-      def request
-        @context.registers[:controller].request
+      def controller
+        @context.registers[:controller]
       end
 
       def view
@@ -59,35 +146,39 @@ module Yoolk
       private
 
         def home_page?
-          request.fullpath == view.root_path
+          controller.request.fullpath == view.root_path
         end
 
         def map_page?
-          request.fullpath.start_with?(view.map_index_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.map_index_path.split('?')[0])
         end
 
         def about_us_page?
-          request.fullpath.start_with?(view.about_us_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.about_us_path.split('?')[0])
         end
 
         def brochures_page?
-          request.fullpath.start_with?(view.brochures_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.brochures_path.split('?')[0])
         end
 
         def people_page?
-          request.fullpath.start_with?(view.people_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.people_path.split('?')[0])
         end
 
         def reservation_page?
-          request.fullpath.start_with?(view.reservation_index_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.reservation_index_path.split('?')[0])
         end
 
         def feedback_page?
-          request.fullpath.start_with?(view.feedback_index_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.feedback_index_path.split('?')[0])
         end
 
         def contact_us_page?
-          request.fullpath.start_with?(view.contact_us_path.split('?')[0])
+          controller.request.fullpath.start_with?(view.contact_us_path.split('?')[0])
+        end
+
+        def collection_category?
+          controller.action_name != 'index'
         end
 
     end
