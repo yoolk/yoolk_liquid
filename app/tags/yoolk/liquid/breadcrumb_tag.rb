70ea906c142926ef868e7e38b2ed8879717a4cd4
radin-reth <radin.reth@gmail.com>
2014-11-21 15:04:07 +0700

    add translation

--- app/tags/yoolk/liquid/breadcrumb_tag.rb
+++ app/tags/yoolk/liquid/breadcrumb_tag.rb
@@ -21,25 +21,25 @@ module Yoolk
       def render(context)
         @context = context
         unless request.home_url?
-          if request.map_url?              then list 'map'
-          elsif request.about_us_url?      then list 'about_us'
-          elsif request.brochures_url?     then list 'brochures'
-          elsif request.people_url?        then list 'people'
-          elsif request.reservation_url?   then list 'reservation'
-          elsif request.feedback_url?      then list 'feedback'
-          elsif request.contact_us_url?    then list 'contact_us'
+          if request.map_url?              then list translate('map')
+          elsif request.about_us_url?      then list translate('about_us')
+          elsif request.brochures_url?     then list translate('brochures')
+          elsif request.people_url?        then list translate('people')
+          elsif request.reservation_url?   then list translate('reservation')
+          elsif request.feedback_url?      then list translate('feedback')
+          elsif request.contact_us_url?    then list translate('contact_us')
           elsif request.galleries_url?
-            list view.link_to_if (@context['gallery']), "galleries", request.galleries_path
+            list view.link_to_if (@context['gallery']), translate('galleries'), request.galleries_path
             if gallery = @context['gallery']
               list gallery.name
             end
           elsif request.announcements_url?
-            list view.link_to_if (@context['announcement']), "announcements", request.announcements_path
+            list view.link_to_if (@context['announcement']), translate('announcements'), request.announcements_path
             if announcement = @context['announcement']
               list announcement.id
             end
           elsif request.products_url?
-            list view.link_to_if (@context['product_category'] || @context['product']), "products", request.products_path
+            list view.link_to_if (@context['product_category'] || @context['product']), translate('products'), request.products_path
             if product_category = @context['product_category']
               list product_category.name
             end
@@ -48,7 +48,7 @@ module Yoolk
               list product.name
             end
           elsif request.services_url?
-            list view.link_to_if (@context['service_category'] || @context['service']), "services", request.services_path
+            list view.link_to_if (@context['service_category'] || @context['service']), translate('services'), request.services_path
             if service_category = @context['service_category']
               list service_category.name
             end
@@ -57,7 +57,7 @@ module Yoolk
               list service.name
             end
           else
-            list view.link_to_if (@context['food_category'] || @context['food']), "menu", request.menu_index_path
+            list view.link_to_if (@context['food_category'] || @context['food']), translate('menu'), request.menu_index_path
             if food_category = @context['food_category']
               list food_category.name
             end
