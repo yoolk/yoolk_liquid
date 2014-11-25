4d1b4153f4d6c4bb82c392eb45c4b6d0cc077c74
radin-reth <radin.reth@gmail.com>
2014-11-25 11:31:54 +0700

    add breadcrumb brochures, people, reservation, feedback, contact_us liquid spec example

--- spec/tags/breadcrumb_tag_spec.rb
+++ spec/tags/breadcrumb_tag_spec.rb
@@ -23,6 +23,41 @@ module Yoolk
         expect_template_result('{% breadcrumb %}', about_us_list, { 'request' => request_drop })
       end
 
+      it '#breadcrumb renders inside /brochures' do
+        allow(request_drop).to receive(:brochures_url?).and_return(true)
+        brochures_list = '<ol class="breadcrumb"><li><a href="/">Home</a></li><li>Brochures</li></ol>'
+
+        expect_template_result('{% breadcrumb %}', brochures_list, { 'request' => request_drop })
+      end
+
+      it '#breadcrumb renders inside /people' do
+        allow(request_drop).to receive(:people_url?).and_return(true)
+        people_list = '<ol class="breadcrumb"><li><a href="/">Home</a></li><li>People</li></ol>'
+
+        expect_template_result('{% breadcrumb %}', people_list, { 'request' => request_drop })
+      end
+
+      it '#breadcrumb renders inside /reservation' do
+        allow(request_drop).to receive(:reservation_url?).and_return(true)
+        reservation_list = '<ol class="breadcrumb"><li><a href="/">Home</a></li><li>Reservation</li></ol>'
+
+        expect_template_result('{% breadcrumb %}', reservation_list, { 'request' => request_drop })
+      end
+
+      it '#breadcrumb renders inside /feedback' do
+        allow(request_drop).to receive(:feedback_url?).and_return(true)
+        feedback_list = '<ol class="breadcrumb"><li><a href="/">Home</a></li><li>Feedback</li></ol>'
+
+        expect_template_result('{% breadcrumb %}', feedback_list, { 'request' => request_drop })
+      end
+
+      it '#breadcrumb renders inside /contact_us' do
+        allow(request_drop).to receive(:contact_us_url?).and_return(true)
+        contact_us_list = '<ol class="breadcrumb"><li><a href="/">Home</a></li><li>Contact us</li></ol>'
+
+        expect_template_result('{% breadcrumb %}', contact_us_list, { 'request' => request_drop })
+      end
+
     end
   end
 end
