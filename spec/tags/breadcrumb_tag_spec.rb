a60c96a55f820fc3749b4790978a5a448c0436e7
radin-reth <radin.reth@gmail.com>
2014-11-25 11:24:33 +0700

    add breadcrumb about us liquid spec example

--- spec/tags/breadcrumb_tag_spec.rb
+++ spec/tags/breadcrumb_tag_spec.rb
@@ -15,6 +15,14 @@ module Yoolk
 
         expect_template_result('{% breadcrumb %}', map_list, { 'request' => request_drop })
       end
+
+      it '#breadcrumb renders inside /about_us' do
+        allow(request_drop).to receive(:about_us_url?).and_return(true)
+        about_us_list = '<ol class="breadcrumb"><li><a href="/">Home</a></li><li>About us</li></ol>'
+
+        expect_template_result('{% breadcrumb %}', about_us_list, { 'request' => request_drop })
+      end
+
     end
   end
 end
