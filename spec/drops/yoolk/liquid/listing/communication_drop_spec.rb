3c8663757689ae0d0ffee57bdb364512bc42cf04
radin-reth <radin.reth@gmail.com>
2014-12-05 18:20:57 +0700

    add communciation list item drop spec

--- spec/drops/yoolk/liquid/listing/communication_drop_spec.rb
+++ spec/drops/yoolk/liquid/listing/communication_drop_spec.rb
@@ -11,6 +11,23 @@ module Yoolk
       it { should have_attribute(:image_url) }
       it { should have_attribute(:created_at) }
       it { should have_attribute(:updated_at) }
+
+      it { should respond_to(:li) }
+      it { should respond_to(:value_with_http) }
+      it { should respond_to(:communication) }
+
+    end
+
+    describe Listing::CommunicationDrop, "view" do
+
+      let(:communication_drop)   { described_class.new(Yoolk::Sandbox::Listing::Communication.new(type: "Branch Office", value: "Yellow Tower - Siem Reap")) }
+
+      before do
+        communication_drop.context = context
+      end
+
+      it { communication_drop.li }
+
     end
   end
 end
