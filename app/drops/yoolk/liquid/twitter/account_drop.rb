96046af5af727043007770c54f32f484bd068a3e
radin-reth <radin.reth@gmail.com>
2014-12-17 18:28:41 +0700

    update facebook, twitter drop page url, profile_url

--- app/drops/yoolk/liquid/twitter/account_drop.rb
+++ app/drops/yoolk/liquid/twitter/account_drop.rb
@@ -2,7 +2,7 @@ module Yoolk
   module Liquid
     module Twitter
       class AccountDrop < BaseDrop
-        attributes  :id, :username, :email, :url
+        attributes  :id, :username, :email, :profile_url
 
         def url
           object.profile_url
