330c68339629cb62f0120d959952da266da5f73f
radin-reth <radin.reth@gmail.com>
2014-12-18 11:02:23 +0700

    addthis dynamic image style

--- app/tags/yoolk/liquid/addthis_tag.rb
+++ app/tags/yoolk/liquid/addthis_tag.rb
@@ -1,35 +1,47 @@
 module Yoolk
   module Liquid
     class AddThisTag < ::Liquid::Block
-      Syntax = /(#{::Liquid::VariableSignature}+)/
       attr_accessor :networks
 
       def initialize(tag_name, markup, options)
         super
 
-        @networks = []
-        if markup =~ Syntax
-          @variable_name = $1
-          unless @variable_name == 'follow'
-            raise SyntaxError.new("Syntax Error - Valid syntax: {% addthis 'follow' %}")
-          end
+        @tool, @style = markup.split(',').inject([]) do |props, item|
+          props.push($1.delete("'")) if item =~ /('.*')/
         end
+
+        @networks = []
+        @style_size = case @style
+        when 'large'
+          '32x32'
+        when 'medium'
+          '20x20'
+        when 'small'
+          '16x16'
+        else
+          raise ArgumentError.new("Style must be one of large, medium, small")
+        end
+
+        unless @tool == 'follow'
+          raise SyntaxError.new("Syntax Error - Valid syntax: {% addthis tool: 'follow' %}{% endaddthis %}")
+        end
+
       end
 
       def unknown_tag(name, params, tokens)
         if name.in?(['facebook', 'twitter'])
-          handle_link_tag(params)
+          handle_link_tag(name, params)
         else
           super
         end
       end
 
-      def handle_link_tag(params)
+      def handle_link_tag(name, params)
         args = split_params(params)
         url, size = args.inject([]) do |props, item|
-          props.push($1.delete("\"")) if item =~ /(".*")/
+          props.push($1.delete("'")) if item =~ /('.*')/
         end
-        @networks << { url: url, size: size }
+        @networks << { name: name, url: url, size: size }
       end
 
       def split_params(params)
@@ -40,18 +52,25 @@ module Yoolk
         @context = context
 
         <<-EOF.gsub(/^\s+|$\n/, "")
-          <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+          <div class="addthis_toolbox addthis_default_style addthis_#{ @style_size }_style">
           #{ link_builder }
           </div>
         EOF
 
+        # binding.pry
+        # super.concat("<input>&&&</input>")
+        # '<input>&&&</input>'
       end
 
       # check image, addthis size style
       def link_builder
         @networks.inject("") do |links, li|
-          links.concat(h.link_to "", "", {"addthis-userid" => facebook_page_name(@context["listing.facebook_page.page_name"]), :class=> "addthis_button_facebook_follow"}) if @context["listing.facebook_page"]
-          links.concat(h.link_to "", "", {"addthis-userid" => twitter_page_name(@context["listing.twitter_account.profile_url"]), :class=> "addthis_button_facebook_follow"}) if @context["listing.twitter_account"]
+          case li[:name]
+          when 'facebook'
+            links.concat(h.link_to "", "", {"addthis-userid" => facebook_page_name(@context["listing.facebook_page.page_name"]), :class=> "addthis_button_facebook_follow"})
+          when 'twitter'
+            links.concat(h.link_to "", "", {"addthis-userid" => twitter_page_name(@context["listing.twitter_account.profile_url"]), :class=> "addthis_button_twitter_follow"})
+          end
         end
       end
 
