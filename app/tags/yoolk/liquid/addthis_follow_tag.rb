d731e8119edeb249583acbaef80f7edd01929654
radin-reth <radin.reth@gmail.com>
2014-12-24 09:08:53 +0700

    share addthis base for addthis_share & addthis_follow

--- app/tags/yoolk/liquid/addthis_follow_tag.rb
+++ app/tags/yoolk/liquid/addthis_follow_tag.rb
@@ -1,75 +1,17 @@
 module Yoolk
   module Liquid
-    class AddThisFollowTag < ::Liquid::Block
-      Syntax = /(style)\s*:+\s*'\s*(large|medium|small)\s*'|^\s*$/i
-      attr_accessor :networks
+    class AddThisFollowTag < AddThisBaseTag
 
-      def initialize(tag_name, markup, options)
-        super
-
-        @networks = []
-
-        if Syntax =~ markup
-          @style_size = case $2 || 'small'
-          when 'large'  then '32x32'
-          when 'medium' then '20x20'
-          when 'small'  then '16x16'
-          end
-        else
-          raise SyntaxError.new("Syntax Error in tag 'addthis_follow' - Valid syntax: {% addthis_follow style: 'large' %}")
-        end
+      def render(context)
+        super context
       end
 
       def unknown_tag(name, params, tokens)
-        if name.in?(['facebook', 'twitter'])
-          handle_link_tag(name, params)
-        else
-          super
-        end
-      end
-
-      def blank?
-        false
-      end
-
-      def handle_link_tag(name, params)
-        args = split_params(params)
-
-        url, size = args.inject([]) do |props, item|
-          props.push($1.delete("'")) if item =~ /('.*')/
-        end
-
-        @networks << { name: name, url: url, size: size }
-      end
-
-      def split_params(params)
-        params.split(",").map(&:strip)
-      end
-
-      def render(context)
-        @context = context
-
-        <<-EOF.gsub(/^\s+|$\n/, "")
-          <div class="addthis_toolbox addthis_default_style addthis_#{ @style_size }_style">
-          #{ link_builder }
-          </div>
-        EOF
-      end
-
-      def link_builder
-        @networks.inject("") do |links, option|
-          case option[:name]
-          when 'facebook' then links.concat facebook_link( option )
-          when 'twitter'  then links.concat twitter_link ( option )
-          end
-        end
+        super name, params, tokens
       end
 
       private
 
-        def image_tag option
-          option[:size].present? ? h.image_tag(option[:url], size: option[:size]) : h.image_tag(option[:url])
-        end
 
         def facebook_link( option )
           opts = {
@@ -87,10 +29,6 @@ module Yoolk
                  }
 
           h.content_tag(:a, option[:url].present? ? image_tag( option ) : nil, opts)
-        end
-
-        def h
-          @context.registers[:helper]
         end
 
         def facebook_page_name
--- app/tags/yoolk/liquid/addthis_share_tag.rb
+++ app/tags/yoolk/liquid/addthis_share_tag.rb
@@ -1,104 +1,16 @@
 module Yoolk
   module Liquid
-    class AddThisShareTag < ::Liquid::Block
-      Syntax = /(style)\s*:+\s*'\s*(large|medium|small)\s*'|^\s*$/i
-      attr_accessor :networks
+    class AddThisShareTag < AddThisBaseTag
 
-      def initialize(tag_name, markup, options)
-        super
-
-        @networks = []
-
-        if Syntax =~ markup
-          @style_size = case $2 || 'small'
-          when 'large'  then '32x32'
-          when 'medium' then '20x20'
-          when 'small'  then '16x16'
-          end
-        else
-          raise SyntaxError.new("Syntax Error in tag 'addthis_share' - Valid syntax: {% addthis_share style: 'large' %}")
-        end
+      def render(context)
+        super context
       end
 
       def unknown_tag(name, params, tokens)
-        if name.in?(['facebook', 'twitter', 'counter'])
-          handle_link_tag(name, params)
-        else
-          super
-        end
+        super name, params, tokens
       end
-
-      def blank?
-        false
-      end
-
-      def handle_link_tag(name, params)
-        args = split_params(params)
-
-        url, size = args.inject([]) do |props, item|
-          props.push($1.delete("'")) if item =~ /('.*')/
-        end
-
-        @networks << { name: name, url: url, size: size }
-      end
-
-      def split_params(params)
-        params.split(",").map(&:strip)
-      end
-
-      def render(context)
-        @context = context
-
-        <<-EOF.gsub(/^\s+|$\n/, "")
-          <div class="addthis_toolbox addthis_default_style addthis_#{ @style_size }_style">
-          #{ link_builder }
-
-          </div>
-        EOF
-      end
-
-      def link_builder
-        @networks.inject("") do |links, option|
-          case option[:name]
-          when 'facebook' then links.concat facebook_link( option )
-          when 'twitter'  then links.concat twitter_link ( option )
-          when 'counter'  then links.concat addthis_counter
-          end
-        end
-      end
-
-      private
-
-        def image_tag option
-          option[:size].present? ? h.image_tag(option[:url], size: option[:size]) : h.image_tag(option[:url])
-        end
-
-        def facebook_link( option )
-          opts = {
-                   :class   => "addthis_button_facebook"
-                 }
-
-          h.content_tag(:a, option[:url].present? ? image_tag( option ) : nil, opts)
-        end
-
-        def twitter_link( option )
-          opts = {
-                   :class   => "addthis_button_twitter"
-                 }
-
-          h.content_tag(:a, option[:url].present? ? image_tag( option ) : nil, opts)
-        end
-
-        def addthis_counter
-          "<a class='addthis_counter addthis_bubble_style'></a>"
-        end
-
-        def h
-          @context.registers[:helper]
-        end
     end
   end
 end
 
 Liquid::Template.register_tag('addthis_share', Yoolk::Liquid::AddThisShareTag)
-
