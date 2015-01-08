ad4908cd87fe8a787df12ee2c193c3d707cbb279
radin-reth <radin.reth@gmail.com>
2015-01-08 12:01:58 +0700

    add schema breadcrumb product links

--- app/tags/yoolk/liquid/breadcrumb_tag.rb
+++ app/tags/yoolk/liquid/breadcrumb_tag.rb
@@ -25,27 +25,27 @@ module Yoolk
         elsif request.contact_us_url?    then append_li li(t(:contact_us))
         elsif request.galleries_url?
           append_li li_galleries
-          append_li li(gallery.name)     if gallery
+          append_li li(span gallery.name)     if gallery
         elsif request.announcements_url?
           append_li li_announcements
-          append_li li(announcement.id)  if announcement
+          append_li li(span announcement.id)  if announcement
         elsif request.products_url?
           append_li li_products
           append_li li_product_category  if product_category || product
-          append_li li(product.name)     if product
+          append_li li(span product.name)     if product
         elsif request.services_url?
           append_li li_services
           append_li li_service_category  if service_category || service
-          append_li li(service.name)     if service
+          append_li li(span service.name)     if service
         elsif request.menu_url?
           append_li li_menu
-          append_li li_food_category  if food_category || food
-          append_li li(food.name)     if food
+          append_li li_food_category     if food_category || food
+          append_li li(span food.name)        if food
         end
 
         # http://www.danshort.com/HTMLentities/
         <<-EOF.gsub(/^\s+|$\n/, "")
-          <ol class="#{ @class_name || 'breadcrumb' }">
+          <ol class="#{ @class_name || 'breadcrumb' }" itemscope itemtype="http://schema.org/BreadcrumbList">
           #{@lis.join}
           </ol>
         EOF
@@ -53,12 +53,20 @@ module Yoolk
 
       private
 
+        def span(content)
+          view.content_tag :span, content, itemprop: "name"
+        end
+
         def li_home
-          li view.link_to(t(:home), request.root_url)
+          # li view.link_to(t(:home), request.root_url)
+          home = view.link_to(t(:home), request.root_url, itemprop: "item")
+          home.concat meta_position
+
+          li home
         end
 
         def li_galleries
-          li view.link_to_if(gallery, t(:galleries), request.galleries_url)
+          li view.link_to_if(gallery, t(:galleries), request.galleries_url, itemprop: "item")
         end
 
         def li_announcements
@@ -66,11 +74,11 @@ module Yoolk
         end
 
         def li_products
-          li view.link_to_if(product || product_category, t(:products), request.products_url)
+          li view.link_to_if(product || product_category, t(:products), request.products_url, itemprop: "item")
         end
 
         def li_product_category
-          li view.link_to_if(product, product_category.name, request.products_category_url(product_category))
+          li view.link_to_if(product, span(product_category.name), request.products_category_url(product_category), itemprop: "item")
         end
 
         def li_services
@@ -78,7 +86,7 @@ module Yoolk
         end
 
         def li_service_category
-          li view.link_to_if(service, service_category.name, request.services_category_url(service_category))
+          li view.link_to_if(service, span(service_category.name), request.services_category_url(service_category))
         end
 
         def li_menu
@@ -86,13 +94,20 @@ module Yoolk
         end
 
         def li_food_category
-          li view.link_to_if(food, food_category.name, request.menu_category_url(food_category))
+          li view.link_to_if(food, span(food_category.name), request.menu_category_url(food_category))
         end
 
         def li(content)
-          view.content_tag :li do
+          # view.content_tag :li do
+          #   content
+          # end
+          view.content_tag :li, itemscope: "", itemprop: "itemListElement", itemtype: "http://schema.org/ListItem" do
             content
           end
+        end
+
+        def meta_position
+          view.content_tag :meta, nil, itemprop: "position", content: "1"
         end
 
         def append_li(li)
@@ -136,7 +151,8 @@ module Yoolk
         end
 
         def t(key)
-          I18n.t("#{request.theme_name}.breadcrumb.#{key}", default: I18n.t("breadcrumb.#{key}"))
+          text = I18n.t("#{request.theme_name}.breadcrumb.#{key}", default: I18n.t("breadcrumb.#{key}"))
+          view.content_tag :span, text, itemprop: "name"
         end
 
         def view
