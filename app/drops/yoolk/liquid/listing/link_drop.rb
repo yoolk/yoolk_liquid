ca47445e902020d31dbd4a36d42f396bc08985ca
radin-reth <radin.reth@gmail.com>
2015-01-19 10:56:10 +0700

    [#85679260] add liquid link drop model

---
+++ app/drops/yoolk/liquid/listing/link_drop.rb
@@ -0,0 +1,9 @@
+module Yoolk
+  module Liquid
+    class Listing::LinkDrop < BaseDrop
+      attributes  :id, :title, :link, :description,
+                  :created_at, :updated_at
+
+    end
+  end
+end
