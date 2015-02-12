e06c19dd36e552cdc3b12300203f0fb0094672e9
radin-reth <radin.reth@gmail.com>
2015-02-12 09:37:57 +0700

    [#88127426] fix error specs

--- spec/drops/yoolk/liquid/listing/link_drop_spec.rb
+++ spec/drops/yoolk/liquid/listing/link_drop_spec.rb
@@ -10,15 +10,5 @@ module Yoolk
       it { should have_attribute(:created_at) }
       it { should have_attribute(:updated_at) }
     end
-
-    describe Listing::LinkDrop, 'methods' do
-      let(:drop) { described_class.new(double) }
-
-      it '#url' do
-        expect(drop).to receive(:link_url).with(drop)
-
-        drop.url
-      end
-    end
   end
 end
