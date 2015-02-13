15a74d63f97d6db050fac62bd29b582c7bb770c8
radin-reth <radin.reth@gmail.com>
2015-02-13 18:02:11 +0700

    updated announcement detail with updated at

--- app/drops/yoolk/liquid/listing/announcement_drop.rb
+++ app/drops/yoolk/liquid/listing/announcement_drop.rb
@@ -7,12 +7,11 @@ module Yoolk
       belongs_to  :image, class_name: 'Yoolk::Liquid::AttachmentDrop'
 
       def text
-        binding.pry
         h.auto_link(h.simple_format(object.text))
       end
 
       def h
-        ApplicationController.helpers
+        @context.registers[:helper]
       end
 
       def url
