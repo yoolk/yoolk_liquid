c6c85396c0bb898e74fbb72c6d228d5a57e1755c
Chamnap Chhorn <chamnapchhorn@gmail.com>
2015-02-16 15:49:40 +0700

    fix truncate_html filter conflict with its gem

--- app/filters/yoolk/liquid/truncate_html_filter.rb
+++ app/filters/yoolk/liquid/truncate_html_filter.rb
@@ -1,6 +1,7 @@
 module Yoolk
   module Liquid
     module TruncateHtmlFilter
+      include ::TruncateHtmlHelper
 
       # Truncate html content from the gem `truncate_html`.
       # @param [ Hash ] options
@@ -9,12 +10,13 @@ module Yoolk
       # length: 100
       # omission: '...'
       # word_boundary: /\S/
-      def truncate_html(html, options={})
+      def truncate_html_with_indifferent_access(html, options={})
         return '' if html.nil?
 
         html_string = TruncateHtml::HtmlString.new(html)
         TruncateHtml::HtmlTruncator.new(html_string, options.with_indifferent_access).truncate.html_safe
       end
+      alias_method_chain :truncate_html, :indifferent_access
     end
   end
 end
