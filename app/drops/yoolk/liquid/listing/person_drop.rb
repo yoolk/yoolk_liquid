5c8804697758059b21c8a0c956dc2965c24832b8
radin-reth <radin.reth@gmail.com>
2015-02-17 16:44:18 +0700

    add liquid people default photo

--- app/drops/yoolk/liquid/listing/person_drop.rb
+++ app/drops/yoolk/liquid/listing/person_drop.rb
@@ -12,6 +12,36 @@ module Yoolk
       has_many    :languages,       with: 'Yoolk::Liquid::LanguageDrop'
 
       delegate    :male?, :female?, :full_name, to: :object
+
+      def photo
+        object.photo || default_photo
+      end
+
+      private
+
+        def default_photo
+          h = {
+            "content_type" => "image/png",
+            "attachment_meta" => {
+              "medium" => {
+                "width" => 206,
+                "height" => 239,
+                "url" => view.asset_path("default-images/people/medium/#{ gender.to_s.downcase }_#{ position_level.name.to_s.downcase }.png")
+              },
+              "small" => {
+                "width" => 139,
+                "height" => 161,
+                "url" => view.asset_path("default-images/people/small/#{ gender.to_s.downcase }_#{ position_level.name.to_s.downcase }.png")
+              }
+            }
+          }
+
+          Yoolk::Sandbox::Attachment.new(h)
+        end
+
+        def view
+          @context.registers[:view]
+        end
     end
   end
 end
