4b98d6557d0f7fd1373dd42c0ab1cbb6779fb717
radin-reth <radin.reth@gmail.com>
2015-02-26 15:54:55 +0700

    [#89071038] update custom page method show, url?

--- app/drops/yoolk/liquid/instant_website/custom_page_drop.rb
+++ app/drops/yoolk/liquid/instant_website/custom_page_drop.rb
@@ -5,6 +5,35 @@ module Yoolk
         attributes  :id, :name, :created_at, :updated_at
 
         belongs_to  :template_page,      with: 'Yoolk::Liquid::InstantWebsite::TemplatePage'
+
+        def url
+          page_name = template_page.name.parameterize.underscore
+          page = case page_name
+                 when "menu", "reservation", "feedback" then "#{page_name}_index"
+                 else page_name end
+
+          eval "view.#{page}_path"
+        end
+
+        def show?
+          page_name = template_page.name.parameterize.underscore
+          collection = case page_name
+                 when "brochures" then "artworks"
+                 when "about_us"  then "catalog_items"
+                 when "videos"    then "medias"
+                 else page_name   end
+
+          unless page_name.in? ["products", "menu", "services", "reservation", "feedback", "contact_us"]
+            @context["request"].preview_mode? or \
+            eval %{@context['listing'].#{collection}.present?}
+          else
+            true
+          end
+        end
+
+        def view
+          @context.registers[:view]
+        end
       end
     end
   end
