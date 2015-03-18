20bbb43c157c756da5579b41bafa9cd720c66068
Vuthy San <saravuthy@gmail.com>
2015-03-18 13:58:44 +0700

    change price to money filter

---
+++ app/filters/yoolk/liquid/money_filter.rb
@@ -0,0 +1,17 @@
+module Yoolk
+  module Liquid
+    module MoneyFilter
+      def money(money)
+        return '' if money.nil?
+        sprintf("$%.2f", money)
+      end
+
+      def money_with_currency(money)
+        return '' if money.nil?
+        sprintf("$%.2f USD", money)
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter Yoolk::Liquid::MoneyFilter
---
+++ spec/filters/money_filter_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+module Yoolk
+  module Liquid
+    describe MoneyFilter do
+      it "#money" do
+        expect_template_result("{{ 12.04 | money }}", "$12.04")
+      end
+
+      it "#money_with_currency" do
+        expect_template_result("{{ 12.04 | money_with_currency }}", "$12.04 USD")
+      end
+    end
+  end
+end
