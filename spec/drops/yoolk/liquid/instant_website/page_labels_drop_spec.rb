5814ca311203e614dcd3722ca91e003ba9fd95d3
radin-reth <radin.reth@gmail.com>
2015-03-25 11:39:29 +0700

    [#89071038] remove page labels drop spec method #more?

--- spec/drops/yoolk/liquid/instant_website/page_labels_drop_spec.rb
+++ spec/drops/yoolk/liquid/instant_website/page_labels_drop_spec.rb
@@ -7,28 +7,5 @@ module Yoolk
       it { should have_scope(:more) }
       it { should respond_to(:more?) }
     end
-    describe InstantWebsite::PageLabelsDrop, :focus do
-      let(:request) { Yoolk::Liquid::RequestDrop.new }
-      let(:more)    { binding.pry }
-
-      it 'return true when in preview mode or at least one object of collection more is not empty' do
-        h = {
-            name: "About Us",
-            template_page: {
-              name: "About Us",
-              display_order: 1
-            }
-          }
-        Yoolk::Sandbox::InstantWebsite::PageLabel.new(h)
-
-        # communication.type  = 'Branch Office'
-        # communication.label = 'Yellow Tower - Siem Reap'
-
-        # expect(subject.li).to eq('<span>Yellow Tower - Siem Reap</span>')
-      end
-
-      it 'return false when all objects in collection more are empty and not in preview mode'  do
-      end
-    end
   end
 end
