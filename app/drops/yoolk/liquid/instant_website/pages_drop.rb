37cdb936a060bc12af4d3cb3c7b3ec68f28fabce
radin-reth <radin.reth@gmail.com>
2015-04-01 22:47:19 +0700

    update logic pages drop

--- app/drops/yoolk/liquid/instant_website/pages_drop.rb
+++ app/drops/yoolk/liquid/instant_website/pages_drop.rb
@@ -2,15 +2,20 @@ module Yoolk
   module Liquid
     class InstantWebsite::PagesDrop < ::Liquid::Rails::CollectionDrop
       def primary
-        @primary ||= self.class.new(objects.select { |object| object.primary? }.take(6))
+        # @primary ||= self.class.new(objects.select { |object| object.primary? }.take(6))
+        @primary ||= self.class.new(objects.take(5))
       end
 
       def more
-        @more ||= self.class.new(objects.select { |object| !object.primary? }.drop(6))
+        # @more ||= self.class.new(objects.select { |object| !object.primary? }.drop(6))
+        @more ||= self.class.new(objects.drop(5))
       end
 
       def more?
-        more.select { |m| m.show? }.present?
+        more.select do |m|
+          m.context = @context
+          m.show?
+        end.present?
       end
     end
   end
