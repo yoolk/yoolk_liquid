b2e4dbd588bd987e3f8ba007e1a307a64e350431
radin-reth <radin.reth@gmail.com>
2015-04-08 10:16:08 +0700

    add draftstamp spec, for listing from groow

--- spec/tags/yoolk/liquid/draft_stamp_tag_spec.rb
+++ spec/tags/yoolk/liquid/draft_stamp_tag_spec.rb
@@ -57,6 +57,25 @@ module Yoolk
         expected_result = "<img alt=\"New draft\" src=\"http://test.host/new-draft.png\" style=\"position: absolute; top: 0; right: 0; z-index: 9999;\" />"
         expect_template_result('{% draft_stamp "new-draft.png" %}', expected_result, listing)
       end
+
+      context '#render' do
+
+        it 'should not render draft stamp for groow listing' do
+          listing = {
+                    'listing'=> {
+                      'from_groow?' => true,
+                      'instant_website'=> {
+                        'is_live'=>false,
+                        'domains'=> [{
+                          'type'=> 'pointed',
+                          'name'=> 'test.host'
+                        }]
+                      }
+                    }
+                  }
+          expect_template_result('{% draft_stamp "new-draft.png" %}', "", listing)
+        end
+      end
     end
   end
 end
