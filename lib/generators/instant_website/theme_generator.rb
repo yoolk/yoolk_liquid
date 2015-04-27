5f0bcf9f25518224aaef2df215b3eb69306be05f
Chamnap Chhorn <chamnapchhorn@gmail.com>
2015-04-27 15:27:16 +0700

    theme_generator generates with pages

--- lib/generators/instant_website/theme_generator.rb
+++ lib/generators/instant_website/theme_generator.rb
@@ -3,28 +3,46 @@ require 'rails/generators/base'
 module InstantWebsite
   module Generators
     class ThemeGenerator < ::Rails::Generators::Base
-      source_root   File.expand_path("../templates", __FILE__)
-      argument      :theme_name, type: :string
-      class_option  :theme_directory, type: :string, default: 'app/themes', desc: 'Theme Directory'
-      desc          "Generates a new blank theme"
+      source_root   File.expand_path('../templates', __FILE__)
+      argument      :theme_name,      type: :string
+      class_option  :theme_directory, type: :string, default: 'app/themes',      desc: 'Theme Directory'
+      class_option  :pages,           type: :string, default: 'home,contact_us', desc: 'Available pages: about_us,announcements,brochures,contact_us,feedback,galleries,home,map,menu,people,products,reservation,services'
+
+      desc          'Generates a new blank theme'
 
      def create_theme_directory
-        empty_directory theme_views_layout
+        empty_directory theme_views_layout_directory
         empty_directory theme_images_directory
         empty_directory theme_javascripts_directory
         empty_directory theme_stylesheets_directory
         empty_directory theme_locales_directory
-        create_file     "#{theme_images_directory}/.gitkeep", nil
-        create_file     "#{theme_locales_directory}/.gitkeep", nil
+        create_file     "#{theme_images_directory}/.gitkeep",   nil
+        create_file     "#{theme_locales_directory}/.gitkeep",  nil
       end
 
       def copy_manifest_files
-        copy_file       "all.js.coffee",  "#{theme_javascripts_directory}/all.js.coffee"
-        copy_file       "all.scss",       "#{theme_stylesheets_directory}/all.scss"
+        copy_file       'all.js.coffee',  "#{theme_javascripts_directory}/all.js.coffee"
+        copy_file       'all.scss',       "#{theme_stylesheets_directory}/all.scss"
       end
 
       def copy_layout_file
-        template "layout.liquid", "#{theme_views_layout}/#{theme_name}.liquid"
+        template        'layout.liquid',  "#{theme_views_layout_directory}/#{theme_name}.liquid"
+      end
+
+      def create_pages
+        create_about_us       if pages.include?('about_us')
+        create_announcements  if pages.include?('announcements')
+        create_brochures      if pages.include?('brochures')
+        create_contact_us     if pages.include?('contact_us')
+        create_feedback       if pages.include?('feedback')
+        create_galleries      if pages.include?('galleries')
+        create_home           if pages.include?('home')
+        create_map            if pages.include?('map')
+        create_menu           if pages.include?('menu')
+        create_people         if pages.include?('people')
+        create_products       if pages.include?('products')
+        create_reservation    if pages.include?('reservation')
+        create_services       if pages.include?('services')
       end
 
       private
@@ -33,8 +51,80 @@ module InstantWebsite
           "#{options.theme_directory}/#{theme_name}"
         end
 
-        def theme_views_layout
-          "#{theme_directory}/views/layouts"
+        def theme_views_directory
+          "#{theme_directory}/views"
+        end
+
+        def theme_views_home_directory
+          "#{theme_views_directory}/home"
+        end
+
+        def theme_views_contact_us_directory
+          "#{theme_views_directory}/contact_us"
+        end
+
+        def theme_views_about_us_directory
+          "#{theme_views_directory}/about_us"
+        end
+
+        def theme_views_announcements_directory
+          "#{theme_views_directory}/announcements"
+        end
+
+        def theme_views_brochures_directory
+          "#{theme_views_directory}/brochures"
+        end
+
+        def theme_views_feedback_directory
+          "#{theme_views_directory}/feedback"
+        end
+
+        def theme_views_galleries_directory
+          "#{theme_views_directory}/galleries"
+        end
+
+        def theme_views_map_directory
+          "#{theme_views_directory}/map"
+        end
+
+        def theme_views_menu_directory
+          "#{theme_views_directory}/menu"
+        end
+
+        def theme_views_menu_foods_directory
+          "#{theme_views_menu_directory}/foods"
+        end
+
+        def theme_views_menu_categories_directory
+          "#{theme_views_menu_directory}/categories"
+        end
+
+        def theme_views_people_directory
+          "#{theme_views_directory}/people"
+        end
+
+        def theme_views_products_directory
+          "#{theme_views_directory}/products"
+        end
+
+        def theme_views_products_categories_directory
+          "#{theme_views_products_directory}/categories"
+        end
+
+        def theme_views_reservation_directory
+          "#{theme_views_directory}/reservation"
+        end
+
+        def theme_views_services_directory
+          "#{theme_views_directory}/services"
+        end
+
+        def theme_views_services_categories_directory
+          "#{theme_views_services_directory}/categories"
+        end
+
+        def theme_views_layout_directory
+          "#{theme_views_directory}/layouts"
         end
 
         def theme_images_directory
@@ -51,6 +141,86 @@ module InstantWebsite
 
         def theme_locales_directory
           "#{theme_directory}/locales"
+        end
+
+        def pages
+          options.pages.split(',')
+        end
+
+        def create_home
+          empty_directory theme_views_home_directory
+          create_file     "#{theme_views_home_directory}/index.liquid",       "<h1>Home#index</h1>\n<p>Find me in #{theme_views_home_directory}/index.liquid"
+        end
+
+        def create_contact_us
+          empty_directory theme_views_contact_us_directory
+          create_file     "#{theme_views_contact_us_directory}/index.liquid", "<h1>ContactUs#index</h1>\n<p>Find me in #{theme_views_contact_us_directory}/index.liquid"
+        end
+
+        def create_about_us
+          empty_directory theme_views_about_us_directory
+          create_file     "#{theme_views_about_us_directory}/index.liquid", "<h1>AboutUs#index</h1>\n<p>Find me in #{theme_views_about_us_directory}/index.liquid"
+        end
+
+        def create_announcements
+          empty_directory theme_views_announcements_directory
+          create_file     "#{theme_views_announcements_directory}/index.liquid", "<h1>Announcements#index</h1>\n<p>Find me in #{theme_views_announcements_directory}/index.liquid"
+        end
+
+        def create_brochures
+          empty_directory theme_views_brochures_directory
+          create_file     "#{theme_views_brochures_directory}/index.liquid", "<h1>Brochures#index</h1>\n<p>Find me in #{theme_views_brochures_directory}/index.liquid"
+        end
+
+        def create_feedback
+          empty_directory theme_views_feedback_directory
+          create_file     "#{theme_views_feedback_directory}/index.liquid", "<h1>Feedback#index</h1>\n<p>Find me in #{theme_views_feedback_directory}/index.liquid"
+        end
+
+        def create_galleries
+          empty_directory theme_views_galleries_directory
+          create_file     "#{theme_views_galleries_directory}/index.liquid", "<h1>Galleries#index</h1>\n<p>Find me in #{theme_views_galleries_directory}/index.liquid"
+          create_file     "#{theme_views_galleries_directory}/show.liquid", "<h1>Galleries#show</h1>\n<p>Find me in #{theme_views_galleries_directory}/show.liquid"
+        end
+
+        def create_map
+          empty_directory theme_views_map_directory
+          create_file     "#{theme_views_map_directory}/index.liquid", "<h1>Map#index</h1>\n<p>Find me in #{theme_views_map_directory}/index.liquid"
+        end
+
+        def create_menu
+          empty_directory theme_views_menu_directory
+          empty_directory theme_views_menu_foods_directory
+          empty_directory theme_views_menu_categories_directory
+          create_file     "#{theme_views_menu_directory}/index.liquid", "<h1>Menu#index</h1>\n<p>Find me in #{theme_views_menu_directory}/index.liquid"
+          create_file     "#{theme_views_menu_foods_directory}/show.liquid", "<h1>Menu/Foods#show</h1>\n<p>Find me in #{theme_views_menu_foods_directory}/show.liquid"
+          create_file     "#{theme_views_menu_categories_directory}/show.liquid", "<h1>Menu/Categories#show</h1>\n<p>Find me in #{theme_views_menu_categories_directory}/show.liquid"
+        end
+
+        def create_people
+          empty_directory theme_views_people_directory
+          create_file     "#{theme_views_people_directory}/index.liquid", "<h1>People#index</h1>\n<p>Find me in #{theme_views_people_directory}/index.liquid"
+        end
+
+        def create_products
+          empty_directory theme_views_products_directory
+          empty_directory theme_views_products_categories_directory
+          create_file     "#{theme_views_products_directory}/index.liquid", "<h1>Products#index</h1>\n<p>Find me in #{theme_views_products_directory}/index.liquid"
+          create_file     "#{theme_views_products_directory}/show.liquid", "<h1>Products#show</h1>\n<p>Find me in #{theme_views_products_directory}/show.liquid"
+          create_file     "#{theme_views_products_categories_directory}/show.liquid", "<h1>Products/Categories#show</h1>\n<p>Find me in #{theme_views_products_categories_directory}/show.liquid"
+        end
+
+        def create_reservation
+          empty_directory theme_views_reservation_directory
+          create_file     "#{theme_views_reservation_directory}/index.liquid", "<h1>Reservation#index</h1>\n<p>Find me in #{theme_views_reservation_directory}/index.liquid"
+        end
+
+        def create_services
+          empty_directory theme_views_services_directory
+          empty_directory theme_views_services_categories_directory
+          create_file     "#{theme_views_services_directory}/index.liquid", "<h1>Services#index</h1>\n<p>Find me in #{theme_views_services_directory}/index.liquid"
+          create_file     "#{theme_views_services_directory}/show.liquid", "<h1>Services#show</h1>\n<p>Find me in #{theme_views_services_directory}/show.liquid"
+          create_file     "#{theme_views_services_categories_directory}/show.liquid", "<h1>Services/Categories#show</h1>\n<p>Find me in #{theme_views_services_categories_directory}/show.liquid"
         end
     end
   end
--- spec/generators/instant_website/theme_generator_spec.rb
+++ spec/generators/instant_website/theme_generator_spec.rb
@@ -7,48 +7,51 @@ describe InstantWebsite::Generators::ThemeGenerator do
   # Tell the generator where to put its output (what it thinks of as Rails.root)
   destination "#{Rails.root}/tmp"
 
-  before do
-    prepare_destination
-    run_generator %w(theme_a)
-  end
+  before      { prepare_destination }
 
-  it "exists `app/themes`" do
-    expect(file("app/themes")).to exist
-  end
+  context 'default' do
+    before   { run_generator %w(theme_a) }
 
-  it "exists `app/themes/theme_a`" do
-    expect(file("app/themes/theme_a")).to exist
-  end
+    it "exists `app/themes`" do
+      expect(file("app/themes")).to exist
+    end
 
-  it "exists `app/themes/theme_a/locales`" do
-    expect(file("app/themes/theme_a/locales")).to exist
-  end
+    it "exists `app/themes/theme_a`" do
+      expect(file("app/themes/theme_a")).to exist
+    end
 
-  it "exists `app/themes/theme_a/assets/images/theme_a`" do
-    expect(file("app/themes/theme_a/assets/images/theme_a")).to exist
-  end
+    it "exists `app/themes/theme_a/locales`" do
+      expect(file("app/themes/theme_a/locales")).to exist
+    end
 
-  it "exists `app/themes/theme_a/assets/javascripts/theme_a`" do
-    expect(file("app/themes/theme_a/assets/javascripts/theme_a")).to exist
-  end
+    it "exists `app/themes/theme_a/assets/images/theme_a`" do
+      expect(file("app/themes/theme_a/assets/images/theme_a")).to exist
+    end
 
-  it "exists `app/themes/theme_a/assets/javascripts/theme_a/all.js.coffee`" do
-    expect(file("app/themes/theme_a/assets/javascripts/theme_a/all.js.coffee")).to exist
-  end
+    it "exists `app/themes/theme_a/assets/javascripts/theme_a`" do
+      expect(file("app/themes/theme_a/assets/javascripts/theme_a")).to exist
+    end
 
-  it "exists `app/themes/theme_a/assets/stylesheets/theme_a`" do
-    expect(file("app/themes/theme_a/assets/stylesheets/theme_a")).to exist
-  end
+    it "exists `app/themes/theme_a/assets/javascripts/theme_a/all.js.coffee`" do
+      expect(file("app/themes/theme_a/assets/javascripts/theme_a/all.js.coffee")).to exist
+    end
 
-  it "exists `app/themes/theme_a/assets/stylesheets/theme_a/all.scss`" do
-    expect(file("app/themes/theme_a/assets/stylesheets/theme_a/all.scss")).to exist
+    it "exists `app/themes/theme_a/assets/stylesheets/theme_a`" do
+      expect(file("app/themes/theme_a/assets/stylesheets/theme_a")).to exist
+    end
+
+    it "exists `app/themes/theme_a/assets/stylesheets/theme_a/all.scss`" do
+      expect(file("app/themes/theme_a/assets/stylesheets/theme_a/all.scss")).to exist
+    end
   end
 
   context "layout file: liquid" do
+    before        { run_generator %w(theme_a) }
+
     before(:all)  {
       Rails.configuration.app_generators.rails[:template_engine] = :liquid
     }
-    after(:all)  {
+    after(:all)   {
       Rails.configuration.app_generators.rails[:template_engine] = :haml
     }
     subject { file("app/themes/theme_a/views/layouts/theme_a.liquid") }
@@ -60,5 +63,97 @@ describe InstantWebsite::Generators::ThemeGenerator do
     it { should contain(/\{\{ content_for_layout \}\}/) }
     it { should contain(/\{% draft_stamp %\}/) }
     it { should contain(/<body data-theme-name="{{ request.theme_name }}" data-listing-alias-id="{{ listing.alias_id }}" data-class-name="{{ request.js_class_name }}">/) }
+  end
+
+  context "create pages" do
+    before   { run_generator %w(theme_a --pages about_us,announcements,brochures,contact_us,feedback,galleries,home,map,menu,people,products,reservation,services) }
+
+    it "`home/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/home/index.liquid')).to exist
+    end
+
+    it "`contact_us/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/contact_us/index.liquid')).to exist
+    end
+
+    it "`about_us/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/about_us/index.liquid')).to exist
+    end
+
+    it "`announcements/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/announcements/index.liquid')).to exist
+    end
+
+    it "`brochures/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/brochures/index.liquid')).to exist
+    end
+
+    it "`feedback/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/feedback/index.liquid')).to exist
+    end
+
+    context "Galleries" do
+      it "`galleries/index.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/galleries/index.liquid')).to exist
+      end
+
+      it "`galleries/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/galleries/show.liquid')).to exist
+      end
+    end
+
+    it "`map/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/map/index.liquid')).to exist
+    end
+
+    context "Menu" do
+      it "`menu/index.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/menu/index.liquid')).to exist
+      end
+
+      it "`menu/categories/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/menu/categories/show.liquid')).to exist
+      end
+
+      it "`menu/foods/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/menu/foods/show.liquid')).to exist
+      end
+    end
+
+    it "`people/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/people/index.liquid')).to exist
+    end
+
+    context "Products" do
+      it "`products/index.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/products/index.liquid')).to exist
+      end
+
+      it "`products/categories/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/products/categories/show.liquid')).to exist
+      end
+
+      it "`products/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/products/show.liquid')).to exist
+      end
+    end
+
+    it "`reservation/index.liquid` should exist" do
+      expect(file('app/themes/theme_a/views/reservation/index.liquid')).to exist
+    end
+
+    context "Services" do
+      it "`services/index.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/services/index.liquid')).to exist
+      end
+
+      it "`services/categories/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/services/categories/show.liquid')).to exist
+      end
+
+      it "`services/show.liquid` should exist" do
+        expect(file('app/themes/theme_a/views/services/show.liquid')).to exist
+      end
+    end
   end
 end
