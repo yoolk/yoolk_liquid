37b1ebe31051c5eeeb40d3481b50fd1ddbf7cb6c
kuyseng <kuyseng@gmail.com>
2015-05-11 16:49:40 +0700

    add page object for customized-menu-label

--- spec/drops/yoolk/liquid/instant_website/template_page_drop_spec.rb
+++ spec/drops/yoolk/liquid/instant_website/template_page_drop_spec.rb
@@ -9,9 +9,22 @@ module Yoolk
       it { should have_attribute(:created_at) }
       it { should have_attribute(:updated_at) }
 
-      it { should delegate(:menu?).to(:object) }
+      it { should delegate(:home?).to(:object) }
       it { should delegate(:products?).to(:object) }
       it { should delegate(:services?).to(:object) }
+      it { should delegate(:menu?).to(:object) }
+      it { should delegate(:galleries?).to(:object) }
+      it { should delegate(:about_us?).to(:object) }
+      it { should delegate(:contact_us?).to(:object) }
+      it { should delegate(:reservation?).to(:object) }
+      it { should delegate(:feedback?).to(:object) }
+      it { should delegate(:announcements?).to(:object) }
+      it { should delegate(:videos?).to(:object) }
+      it { should delegate(:attachments?).to(:object) }
+      it { should delegate(:links?).to(:object) }
+      it { should delegate(:people?).to(:object) }
+      it { should delegate(:map?).to(:object) }
+      it { should delegate(:brochures?).to(:object) }
     end
   end
 end
--- spec/models/yoolk/sandbox/instant_website/template_page_spec.rb
+++ spec/models/yoolk/sandbox/instant_website/template_page_spec.rb
@@ -28,9 +28,9 @@ module Yoolk
       it { should respond_to(:about_us?) }
       it { should respond_to(:map?) }
 
-      it '#menu? return true when navigate to menu page' do
-        page = InstantWebsite::TemplatePage.new(name: 'Menu')
-        expect(page.menu?).to eq(true)
+      it '#home? return true when navigate to home page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Home')
+        expect(page.home?).to eq(true)
       end
 
       it '#products? return true when navigate to products page' do
@@ -43,15 +43,71 @@ module Yoolk
         expect(page.services?).to eq(true)
       end
 
+      it '#menu? return true when navigate to menu page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Menu')
+        expect(page.menu?).to eq(true)
+      end
+
+      it '#galleries? return true when navigate to galleries page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Galleries')
+        expect(page.galleries?).to eq(true)
+      end
+
       it '#about_us? return true when navigate to about us page' do
         page = InstantWebsite::TemplatePage.new(name: 'About Us')
         expect(page.about_us?).to eq(true)
+      end
+
+      it '#contact_us? return true when navigate to contact_us page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Contact Us')
+        expect(page.contact_us?).to eq(true)
+      end
+
+      it '#reservation? return true when navigate to reservation page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Reservation')
+        expect(page.reservation?).to eq(true)
+      end
+
+      it '#feedback? return true when navigate to feedback page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Feedback')
+        expect(page.feedback?).to eq(true)
+      end
+
+      it '#announcements? return true when navigate to announcements page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Announcements')
+        expect(page.announcements?).to eq(true)
+      end
+
+      it '#videos? return true when navigate to videos page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Videos')
+        expect(page.videos?).to eq(true)
+      end
+
+      it '#attachments? return true when navigate to attachments page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Attachments')
+        expect(page.attachments?).to eq(true)
+      end
+
+      it '#links? return true when navigate to links page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Links')
+        expect(page.links?).to eq(true)
+      end
+
+      it '#people? return true when navigate to people page' do
+        page = InstantWebsite::TemplatePage.new(name: 'People')
+        expect(page.people?).to eq(true)
       end
 
       it '#map? return true when navigate to map page' do
         page = InstantWebsite::TemplatePage.new(name: 'Map')
         expect(page.map?).to eq(true)
       end
+
+      it '#Brochures? return true when navigate to brochures page' do
+        page = InstantWebsite::TemplatePage.new(name: 'Brochures')
+        expect(page.brochures?).to eq(true)
+      end
+
     end
   end
 end
