020a1552d9f939d00dc71c37402b2a976fb133d4
radin-reth <radin.reth@gmail.com>
2015-07-01 10:46:16 +0700

    add specs

--- spec/models/yoolk/sandbox/instant_website/page_spec.rb
+++ spec/models/yoolk/sandbox/instant_website/page_spec.rb
@@ -11,6 +11,22 @@ module Yoolk
         expect(described_class).to have_attribute(:name).of_type(String)
       end
 
+      it 'has :display_order attribute' do
+        expect(described_class).to have_attribute(:display_order).of_type(Integer)
+      end
+
+      it 'has :meta_title attribute' do
+        expect(described_class).to have_attribute(:meta_title).of_type(String)
+      end
+
+      it 'has :meta_description attribute' do
+        expect(described_class).to have_attribute(:meta_description).of_type(String)
+      end
+
+      it 'has :meta_keyword attribute' do
+        expect(described_class).to have_attribute(:meta_keyword).of_type(String)
+      end
+
       it 'has :template_page attribute' do
         expect(described_class).to have_attribute(:template_page).of_type(Yoolk::Sandbox::InstantWebsite::TemplatePage)
       end
--- spec/models/yoolk/sandbox/instant_website/website_spec.rb
+++ spec/models/yoolk/sandbox/instant_website/website_spec.rb
@@ -19,6 +19,10 @@ module Yoolk
         expect(described_class).to have_attribute(:google_remarketing_tag).of_type(String)
       end
 
+      it 'has :google_webmaster_tag attribute' do
+        expect(described_class).to have_attribute(:google_webmaster_tag).of_type(String)
+      end
+
       it 'has :is_live attribute' do
         expect(described_class).to have_attribute(:is_live)
       end
