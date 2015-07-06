a519774f5c29f1e8b2b5c79f22dc5005e5c5af99
Chamnap Chhorn <chamnapchhorn@gmail.com>
2015-07-06 10:33:08 +0700

    memorize

--- app/models/yoolk/sandbox/listing/catalog_items.rb
+++ app/models/yoolk/sandbox/listing/catalog_items.rb
@@ -2,11 +2,11 @@ module Yoolk
   module Sandbox
     class Listing::CatalogItems < Collection
       def published
-        self.class.new(select { |item| item.published? })
+        @published   ||= self.class.new(select { |item| item.published? })
       end
 
       def with_images
-        self.class.new(select { |item| item.image.present? })
+        @with_images ||= self.class.new(select { |item| item.image.present? })
       end
     end
   end
--- app/models/yoolk/sandbox/listing/image_galleries.rb
+++ app/models/yoolk/sandbox/listing/image_galleries.rb
@@ -2,7 +2,7 @@ module Yoolk
   module Sandbox
     class Listing::ImageGalleries < Collection
       def with_images
-        self.class.new(select { |gallery| gallery.images.present? })
+        @with_images ||= self.class.new(select { |gallery| gallery.images.present? })
       end
     end
   end
