254983a02565acb63512f28334e69e0c722d98b2
radin-reth <radin.reth@gmail.com>
2015-08-14 17:29:47 +0700

    delegate new methods for product drop

--- app/drops/yoolk/liquid/product_catalog/product_drop.rb
+++ app/drops/yoolk/liquid/product_catalog/product_drop.rb
@@ -4,15 +4,17 @@ module Yoolk
       class ProductDrop < BaseDrop
         attributes  :id, :name, :price, :sale_price, :quantity, :price_with_currency,
                     :description, :delivery, :features, :brand, :to_param, :hide_price,
-                    :sku, :storefront, :published, :tracked_inventory, :storefront_order,
-                    :selling_price, :in_stock?, :out_of_stock?, :unlimited?,
+                    :hide_price?, :sku, :storefront, :published, :tracked_inventory,
+                    :storefront_order, :selling_price, :in_stock?,
+                    :out_of_stock?, :unlimited?,
                     :created_at, :updated_at
 
         has_many    :product_categories,  with: 'Yoolk::Liquid::ProductCatalog::CategoryDrop'
         has_many    :photos,              with: 'Yoolk::Liquid::AttachmentDrop'
 
         ## Delegates
-        delegate    :add_to_cart?, :available_for_sale?, to: :object
+        delegate    :add_to_cart?, :available_for_sale?, :show_price?,
+                    to: :object
 
         def url
           product_url(self)
@@ -32,10 +34,6 @@ module Yoolk
               image_url: cover_photo.url(:small)
             }
           }.to_json
-        end
-
-        def show_price?
-          true
         end
       end
     end
