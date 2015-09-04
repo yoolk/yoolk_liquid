60e5afcc0a777776892338a3b4a291c59da78c0d
radin-reth <radin.reth@gmail.com>
2015-09-04 18:28:41 +0700

    render badge for presented value and non-zero

--- app/filters/yoolk/liquid/discount_badge_filter.rb
+++ app/filters/yoolk/liquid/discount_badge_filter.rb
@@ -6,11 +6,13 @@ module Yoolk
       # @param [ value ] float
       # @param [ shape_name ] : [circle, waterdrop, rectangle]
       def discount_badge(value, shape_name='circle')
+        return if value.present? && value != '0%'
+        
         %Q{
           <span class="#{shape_name}-shape" id="#{shape_name}-shape">
             #{value} <small>OFF</small>
           </span>
-        }.gsub(/^\s+|$\n/, '') if value.present?
+        }.gsub(/^\s+|$\n/, '')
       end
     end
   end
