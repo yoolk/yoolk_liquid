8ca10531749ca3d643183dd9b1da1235aacb5652
radin-reth <radin.reth@gmail.com>
2015-10-01 16:07:41 +0700

    add adwords remarketing script, facebook audience pixel, conversion pixel on submitted email script

--- app/drops/yoolk/liquid/content_closing_body.rb
+++ app/drops/yoolk/liquid/content_closing_body.rb
@@ -10,12 +10,10 @@ module Yoolk
 
       def to_s
         return [] if tracking_services.empty? #|| preview_mode?
-
         [
           adwords_remarketing_script,
-          adwords_conversions_script,
-          fb_conversion_pixels_script,
-          fb_custom_audience_pixels_script
+          fb_custom_audience_pixels_script,
+          conversions_pixel_on_submitted_email_script
         ].compact.join("\n")
       end
 
@@ -23,16 +21,53 @@ module Yoolk
 
       def adwords_remarketing_script
         return [] if tracking_services.adwords_remarketing.empty?
-        
-        tracking_services.adwords_remarketing.map(&:script)
+
+        %(
+          <!-- Adwords Remarketing Script -->
+          #{ tracking_services.adwords_remarketing.map(&:script).join("\n").html_safe }
+        )
+      end
+
+      def fb_custom_audience_pixels_script
+        return [] if tracking_services.fb_custom_audience_pixels.empty?
+
+        %(
+          <!-- Facebook Custom Audience Pixels Script -->
+          #{ tracking_services.fb_custom_audience_pixels.map(&:script).join("\n").html_safe }
+        )
+      end
+
+      def conversions_pixel_on_submitted_email_script
+        return [] if no_services_for_submitted_an_email? || !form_posted_successfully?
+
+        [adwords_conversions_for_submitted_an_email, fb_conversion_pixels_for_submitted_an_email].flatten.map(&:script).join("\n".html_safe)
+      end
+
+      def no_services_for_submitted_an_email?
+        adwords_conversions_for_submitted_an_email.empty? && fb_conversion_pixels_for_submitted_an_email.empty?
+      end
+
+      def adwords_conversions_for_submitted_an_email
+        Array.wrap(tracking_services.adwords_conversions.try(:submitted_an_email))
+      end
+
+      def fb_conversion_pixels_for_submitted_an_email
+        Array.wrap(tracking_services.fb_conversion_pixels.try(:submitted_an_email))
       end
 
       def adwords_conversions_script
-        binding.pry
-        if request.home_page?
-          instant_website.tracking_services.adwords_conversions.visited_my_homepages.map(&:script)
-        elsif request.checkout_page?
-
+        if    request.visited_any_page?
+          tracking_services.adwords_conversions.visited_my_homepages.map(&:script)
+        elsif request.added_a_product_to_cart?
+          tracking_services.adwords_conversions.added_a_product_to_cart.map(&:script)
+        elsif request.visited_my_homepage?
+          tracking_services.adwords_conversions.visited_my_homepage.map(&:script)
+        elsif request.clicked_on_checkout?
+          tracking_services.adwords_conversions.clicked_on_checkout.map(&:script)
+        elsif request.place_an_order?
+          tracking_services.adwords_conversions.place_an_order.map(&:script)
+        elsif request.submitted_an_email?
+          tracking_services.adwords_conversions.submitted_an_email.map(&:script)
         end
       end
 
@@ -44,12 +79,6 @@ module Yoolk
         tracking_services.fb_conversion_pixels.map(&:script)
       end
 
-      def fb_custom_audience_pixels
-        return [] if tracking_services.fb_custom_audience_pixels.empty?
-        
-        tracking_services.fb_custom_audience_pixels.map(&:script)
-      end
-
       def tracking_services
         listing.instant_website.try(:tracking_services)
       end
@@ -58,8 +87,8 @@ module Yoolk
         view_context.preview_mode?
       end
 
-      def contact_us_posted_successfully?
-        controller_name.contact_us? && flash.notice.present?
+      def form_posted_successfully?
+        controller_name.in?(["contact_us", "reservation", "feedback"]) && flash.notice.present?
       end
 
       def controller_name
--- app/drops/yoolk/liquid/instant_website/tracking_service_drop.rb
+++ app/drops/yoolk/liquid/instant_website/tracking_service_drop.rb
@@ -3,7 +3,7 @@ module Yoolk
     module InstantWebsite
       class TrackingServiceDrop < BaseDrop
         attributes  :id, :adwords_conversion_type, :adwords_conversion_id, :adwords_conversion_label, 
-                    :adwords_remarketing_id, :facebook_custom_audience_pixel_id, :facebook_conversion_pixel_type
+                    :adwords_remarketing_id, :facebook_custom_audience_pixel_id, :facebook_conversion_pixel_type, :script, 
                     :facebook_conversion_pixel_id, :service_type, :created_at, :updated_at
 
       end
